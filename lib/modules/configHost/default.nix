bc2709d7980b351175938f4b6f94983b333aeed6
Átila Saraiva <atilasaraiva@gmail.com>
2022-04-29 22:47:59 -0300

    increased the tmpfs mem limit to 180pct

--- lib/modules/configHost/default.nix
+++ lib/modules/configHost/default.nix
@@ -269,7 +269,7 @@ in
 
     boot.tmpOnTmpfs = cfg.boot.tmpOnTmpfs;
     boot.cleanTmpDir = !cfg.boot.tmpOnTmpfs;
-    boot.tmpOnTmpfsSize = "90%";
+    boot.tmpOnTmpfsSize = "180%";
     boot.loader.systemd-boot.enable = cfg.boot.loader.systemdBoot.enable;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.timeout = cfg.boot.loader.systemdBoot.timeout;
