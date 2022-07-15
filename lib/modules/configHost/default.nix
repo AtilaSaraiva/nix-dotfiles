bd64514f61b83402c3a7979a80bbe7c9e3406164
Átila Saraiva <atilasaraiva@gmail.com>
2022-07-14 21:45:04 -0300

    disabled wireplumber

--- lib/modules/configHost/default.nix
+++ lib/modules/configHost/default.nix
@@ -726,7 +726,7 @@ in
         alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
-        wireplumber.enable = true;
+        #wireplumber.enable = true;
     };
     hardware.pulseaudio.enable = false;
 
