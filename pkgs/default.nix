f89dc1554c6fcec711b65198d7008ce81dbb1bda
Átila Saraiva <atilasaraiva@gmail.com>
2022-08-12 18:12:01 -0300

    pinning waybar to 0.9.12

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,7 @@ self: super: rec {
   microsoft-edge-dev = super.callPackage (import ./edge).dev { };
   fpm      = super.callPackage ./fpm { };
   fobis      = super.python3Packages.callPackage ./fobis { };
+  waybar      = super.callPackage ./waybar { };
   #distrobox = super.distrobox.overrideAttrs (oldAttrs: {
     #version = "unstable-2022-05-25";
     #src = super.fetchFromGitHub {
