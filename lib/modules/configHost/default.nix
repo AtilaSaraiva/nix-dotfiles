19654d6b034f09666f33e619226ad8d294ca1036
AtilaSaraiva <atilasaraiva@gmail.com>
2022-08-14 09:48:08 -0300

    added udev rule to reduce input latency for DS4 controllers

--- lib/modules/configHost/default.nix
+++ lib/modules/configHost/default.nix
@@ -789,7 +789,9 @@ in
     hardware.steam-hardware.enable = true;
 
     # Rule to increase polling rate of dualshock 4 to reduce input lag
-    services.udev.extraRules = "ACTION=="bind", SUBSYSTEM=="hid", DRIVER=="sony", KERNEL=="*054C:09CC*", ATTR{bt_poll_interval}="1";
+    services.udev.extraRules = ''
+      ACTION=="bind", SUBSYSTEM=="hid", DRIVER=="sony", KERNEL=="*054C:09CC*", ATTR{bt_poll_interval}="1"
+    '';
 
     hardware.opengl.extraPackages32 = [
       pkgs.driversi686Linux.amdvlk
