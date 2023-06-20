18550b83549b00640c2b7156d5bf6066d13933c9
Átila Saraiva <atilasaraiva@gmail.com>
2023-06-20 12:36:50 -0600

    added low latency pipewire configuration

--- lib/modules/configHost/default.nix
+++ lib/modules/configHost/default.nix
@@ -875,10 +875,41 @@ in
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
-        jack.enable = true;
-        wireplumber.enable = true;
     };
     hardware.pulseaudio.enable = false;
+
+    security.rtkit.enable = true;
+
+    environment.etc = let
+      json = pkgs.formats.json {};
+    in {
+      "pipewire/pipewire-pulse.d/92-low-latency.conf".source = json.generate "92-low-latency.conf" {
+        context.modules = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              pulse.min.req = "32/48000";
+              pulse.default.req = "32/48000";
+              pulse.max.req = "32/48000";
+              pulse.min.quantum = "32/48000";
+              pulse.max.quantum = "32/48000";
+            };
+          }
+        ];
+        stream.properties = {
+          node.latency = "32/48000";
+          resample.quality = 1;
+        };
+      };
+      "pipewire/pipewire.d/92-low-latency.conf".source = json.generate "92-low-latency.conf" {
+        context.properties = {
+          default.clock.rate = 48000;
+          default.clock.quantum = 32;
+          default.clock.min-quantum = 32;
+          default.clock.max-quantum = 32;
+        };
+      };
+    };
 
     hardware.sane = {
       enable = true;
